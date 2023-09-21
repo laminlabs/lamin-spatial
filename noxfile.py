b9cbc2c21997e3c645b06e8ebab73237e884b0bf
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-09-21 15:36:09 +0200

    🍱 Upload artifacts

--- noxfile.py
+++ noxfile.py
@@ -1,10 +1,7 @@
 import nox
+from laminci import upload_docs_artifact
 from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
 
-# we'd like to aggregate coverage information across sessions
-# and for this the code needs to be located in the same
-# directory in every github action runner
-# this also allows to break out an installation section
 nox.options.default_venv_backend = "none"
 
 
@@ -19,3 +16,4 @@ def build(session):
     login_testuser1(session)
     run_pytest(session)
     build_docs(session, strict=True)
+    upload_docs_artifact(aws=True)
