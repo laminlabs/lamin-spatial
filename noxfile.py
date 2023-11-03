bcaf2b32e88b860e16660884ff5ec19e8c3553ae
Alex Wolf <f.alexander.wolf@gmail.com>
2023-11-03 18:33:42 +0100

    💚 Fix

--- noxfile.py
+++ noxfile.py
@@ -14,6 +14,6 @@ def lint(session: nox.Session) -> None:
 def build(session):
     session.run(*"pip install -r requirements.txt".split())
     login_testuser1(session)
-    session.run("pytest", "tests")
+    session.run(*"pytest -s tests".split())
     build_docs(session, strict=True)
     upload_docs_artifact(aws=True)
