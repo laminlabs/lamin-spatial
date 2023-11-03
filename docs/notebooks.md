dced15347851a60c30676a9fb0ad5d3b1a3d4cba
Alex Wolf <f.alexander.wolf@gmail.com>
2023-11-03 18:52:48 +0100

    ♻️ Refactor repository (#5)

--- docs/notebooks.md
+++ docs/notebooks.md
@@ -1,23 +1,11 @@
 # Notebooks
 
-## `laminlabs/rxrx`
-
-An SQLite instance containing images, metadata and embeddings of RxRx1; metadata and embeddings of RxRx2:
-
-- {doc}`/rxrx`
-- {doc}`/01-download`
-- {doc}`/02-meta-rxrx1`
-- {doc}`/03-meta-rxrx2`
-- {doc}`/04-images-rxrx1`
-
 ```{toctree}
 :maxdepth: 1
-:hidden:
 
-rxrx
-01-download
-02-meta-rxrx1
-03-meta-rxrx2
-04-images-rxrx1
-images-rxrx1-original
+notebooks/01-download
+notebooks/02-meta-rxrx1
+notebooks/03-meta-rxrx2
+notebooks/04-images-rxrx1
+notebooks/images-rxrx1-original
 ```
--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 import nox
 from laminci import upload_docs_artifact
-from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
+from laminci.nox import build_docs, login_testuser1, run_pre_commit
 
 nox.options.default_venv_backend = "none"
 
@@ -12,8 +12,8 @@ def lint(session: nox.Session) -> None:
 
 @nox.session()
 def build(session):
-    session.run(*"pip install -e .[dev]".split())
+    session.run(*"pip install -r requirements.txt".split())
     login_testuser1(session)
-    run_pytest(session)
+    session.run(*"pytest -s tests".split())
     build_docs(session, strict=True)
     upload_docs_artifact(aws=True)
