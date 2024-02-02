27bc61b07c06c2c95e76c7a50694c5855b8a0312
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-02-02 06:42:06 -0500

    💚 Fix CI (#11)

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,10 @@
 import nox
 from laminci import upload_docs_artifact
+<<<<<<< HEAD
 from laminci.nox import build_docs, run_pre_commit
+=======
+from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
+>>>>>>> 7ee4628 (💚 Fix CI (#11))
 
 nox.options.default_venv_backend = "none"
 
@@ -10,7 +14,24 @@ def lint(session: nox.Session) -> None:
     run_pre_commit(session)
 
 
-@nox.session()
+@nox.session
+def install(session: nox.Session):
+    session.run(*"pip install .[dev]".split())
+    session.run(
+        "pip",
+        "install",
+        "lamindb[jupyter,bionty,aws,postgres] @"
+        " git+https://github.com/laminlabs/lamindb@main",
+    )
+    session.run(
+        "pip",
+        "install",
+        "--no-deps",
+        "lnschema-lamin1 @ git+https://github.com/laminlabs/lnschema-lamin1@main",
+    )
+
+
+@nox.session
 def build(session):
     session.run(
         "uv",
