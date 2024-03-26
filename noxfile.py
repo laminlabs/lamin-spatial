b59616d8b2db52cdfc7f1739c0ed80f68deb8b88
Alex Wolf <f.alexander.wolf@gmail.com>
2024-03-26 13:29:08 +0100

    ♻️ Simplify CI

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 import nox
 from laminci import upload_docs_artifact
-from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
+from laminci.nox import build_docs, run_pre_commit
 
 nox.options.default_venv_backend = "none"
 
@@ -11,25 +11,15 @@ def lint(session: nox.Session) -> None:
 
 
 @nox.session
-def install(session: nox.Session):
-    session.run(*"pip install .[dev]".split())
-    session.run(
-        "pip",
-        "install",
-        "lamindb[jupyter,bionty,aws,postgres] @"
-        " git+https://github.com/laminlabs/lamindb@main",
-    )
-    session.run(
-        "pip",
-        "install",
-        "--no-deps",
-        "lnschema-lamin1 @ git+https://github.com/laminlabs/lnschema-lamin1@main",
-    )
-
-
-@nox.session
 def build(session):
-    login_testuser1(session)
-    run_pytest(session, coverage=False)
-    build_docs(session, strip_prefix=False, strict=True)
+    session.run(
+        "uv",
+        "pip",
+        "install",
+        "--system",
+        "lamindb @ git+https://github.com/laminlabs/lamindb@spatial",
+    )
+    session.run(*"uv pip install --system -r requirements.txt".split())
+    session.run(*"pytest -s tests".split())
+    build_docs(session, strict=True)
     upload_docs_artifact(aws=True)
