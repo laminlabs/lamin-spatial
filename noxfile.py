3dc209ae7e6cf459e7beb4cf173c0bdda40d96dd
Alex Wolf <f.alexander.wolf@gmail.com>
2024-03-28 23:08:08 +0100

    💚 Fix

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 import nox
 from laminci import upload_docs_artifact
-from laminci.nox import build_docs, run_pre_commit
+from laminci.nox import build_docs, login_testuser1, run_pre_commit
 
 nox.options.default_venv_backend = "none"
 
@@ -27,6 +27,7 @@ def build(session):
         "lamindb @ git+https://github.com/laminlabs/lamindb@vitessce",
     )
     session.run(*"uv pip install --system -r requirements.txt".split())
+    login_testuser1(session)
     session.run(*"pytest -s tests".split())
     build_docs(session, strict=True)
     upload_docs_artifact(aws=True)
