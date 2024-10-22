819db7011508798da1e653566edec7a49d2c1b6e
zethson <lukas.heumos@posteo.net>
2024-10-22 10:54:19 +0200

    :art: Branch
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -12,7 +12,7 @@ def lint(session: nox.Session) -> None:
 
 @nox.session
 def build(session):
-    install_lamindb(session, extras="bionty,aws,jupyter")
+    install_lamindb(session, branch="release", extras="bionty,aws,jupyter")
     session.run(*"uv pip install --system wetlab findrefs vitessce starlette".split())
     login_testuser1(session)
     session.run(*"pytest -s tests".split())
