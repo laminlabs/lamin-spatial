7c1a1fc8b7fe01a397d6c851a80358effa498e9c
zethson <lukas.heumos@posteo.net>
2024-12-13 12:50:59 +0100

    :art: Add full test pipeline
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- tests/test_spatialdata_curator.py
+++ tests/test_spatialdata_curator.py
@@ -1,37 +1,61 @@
+import bionty as bt
+import lamindb as ln
 import pytest
+from lamin_spatial import SpatialDataCurator
 from spatialdata.datasets import blobs
 
 
 @pytest.fixture
 def blobs_data():
     sdata = blobs()
+
+    sdata.tables["table"].var.index = [
+        "ENSG00000139618",  # BRCA2
+        "ENSG00000157764",  # BRAF
+        "ENSG00000999999",  # does not exist - to test add_new_from_var_index
+    ]
+    sdata.tables["table"].obs["region"] = ["region 1"] * 13 + ["region 2"] * 13
     sdata.attrs["sample"] = {
         "assay": "VisiumHD",
-        "disease": "Alzheimer disease",
+        "disease": "Alzheimer's dementia",
         "preproc_version": "Space Ranger version 3.0.0",
     }
+
     return sdata
 
 
-def test_spatialdata_creation():
-    pass
+def test_spatialdata_curator(blobs_data):
+    curator = SpatialDataCurator(
+        blobs_data,
+        var_index={"table": bt.Gene.ensembl_gene_id},
+        categoricals={
+            "sample": {
+                "assay": bt.ExperimentalFactor.name,
+                "disease": bt.Disease.name,
+                "developmental_stage": bt.DevelopmentalStage.name,
+            },
+            "table": {"region": ln.ULabel.name},
+        },
+        organism="human",
+    )
 
+    curator.validate()
 
-def test_spatialdata_curate():
-    pass
+    curator.add_new_from_var_index("table")
+    curator.add_new_from(key="developmental_stage", accessor="sample")
 
+    curator.standardize(key="disease", accessor="sample")
 
-def test_spatial_standardize():
-    pass
+    artifact = curator.save_artifact(description="blob spatialdata")
 
+    artifact.describe()
 
-def test_add_new_from_var_index():
-    pass
-
-
-def test_add_new_from():
-    pass
-
-
-def test_save_artifact():
-    pass
+    # clean up
+    artifact.delete(permanent=True)
+    ln.ULabel.filter().delete()
+    bt.ExperimentalFactor.filter().delete()
+    bt.Disease.filter().delete()
+    bt.DevelopmentalStage.filter().delete()
+    bt.Gene.filter().delete()
+    ln.FeatureSet.filter().delete()
+    ln.Feature.filter().delete()
