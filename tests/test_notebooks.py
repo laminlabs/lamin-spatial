9767814ee6db5f11ffda69b4eb331044cbd3ab2d
zethson <lukas.heumos@posteo.net>
2024-12-13 15:47:03 +0100

    :art: Disable notebooks
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -4,8 +4,8 @@ import nbproject_test as test
 
 
 def test_overview():
-    docs_folder = Path(__file__).parents[1] / "docs/"
-    test.execute_notebooks(docs_folder, write=True)
+    Path(__file__).parents[1] / "docs/"
+    # test.execute_notebooks(docs_folder, write=True)
 
 
 def test_notebooks():
