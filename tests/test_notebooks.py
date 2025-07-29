9404993586a5d92168b8f342cb40edd1a9eb8873
Alex Wolf <f.alexander.wolf@gmail.com>
2025-07-29 21:51:53 +0200

    ♻️ Run only Vitessce

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -4,10 +4,10 @@ import nbproject_test as test
 
 
 def test_overview():
-    docs_folder = Path(__file__).parents[1] / "docs/"
+    docs_folder = Path(__file__).parents[1] / "docs/vitessce.ipynb"
     test.execute_notebooks(docs_folder, write=True)
 
 
-def test_notebooks():
-    docs_folder = Path(__file__).parents[1] / "docs/notebooks/"
-    test.execute_notebooks(docs_folder, write=True)
+# def test_notebooks():
+#     docs_folder = Path(__file__).parents[1] / "docs/notebooks/"
+#     test.execute_notebooks(docs_folder, write=True)
