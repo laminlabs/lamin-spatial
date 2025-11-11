d5a135fc491fd6bf4544322b153d62f211d6cefd
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2025-11-11 17:23:10 +0100

    📝 Add comment

--- noxfile.py
+++ noxfile.py
@@ -19,9 +19,9 @@ def lint(session: nox.Session) -> None:
 
 @nox.session
 def build(session):
-    # do not use zarr extra here otherwise spatialdata won't be installed
     install_lamindb(session, branch="main", extras="bionty,gcp,jupyter,zarr")
     run(session, f"uv pip install {SYSTEM} .[dev,use_case]")
+    # vitessce requires zarr<3, and therefore upper limits spatialdata<0.5.0
     run(session, f"uv pip install {SYSTEM} spatialdata<=0.5.0")
     login_testuser1(session)
     run(session, "pytest -s tests")
