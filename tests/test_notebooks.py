f79c25d3d77ffd6ccda6a0f4e88270f15be98659
namsaraeva <a.namsaraeva@tum.de>
2026-01-19 13:59:15 +0100

    ♻️  Last cleanup and revert CI verbosity changes

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -5,13 +5,9 @@ import nbproject_test as test
 
 def test_overview():
     docs_folder = Path(__file__).parents[1] / "docs/"
-    test.execute_notebooks(
-        docs_folder, write=True, print_cells=True, print_outputs=True
-    )
+    test.execute_notebooks(docs_folder, write=True)
 
 
 def test_notebooks():
     docs_folder = Path(__file__).parents[1] / "docs/notebooks/"
-    test.execute_notebooks(
-        docs_folder, write=True, print_cells=True, print_outputs=True
-    )
+    test.execute_notebooks(docs_folder, write=True)
